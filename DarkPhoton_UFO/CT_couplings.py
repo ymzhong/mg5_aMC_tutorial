4ff30ea0e90d0aa1b7006ec41bdee99b1ac1d005
Yi-Ming Zhong <myhoneyclover@hotmail.com>
2021-03-05 17:14:26 -0600

    UFO model for a simple dark photon model

---
+++ DarkPhoton_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.36
+# Mathematica version: 12.0.0 for Linux x86 (64-bit) (April 7, 2019)
+# Date: Thu 4 Mar 2021 16:26:20
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ DarkPhoton_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.36
+# Mathematica version: 12.0.0 for Linux x86 (64-bit) (April 7, 2019)
+# Date: Thu 4 Mar 2021 16:26:20
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+QFD = CouplingOrder(name = 'QFD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
